1827ada6ffbafcaaacfa6bb091bf0532ee8454ac
Boubacar Beye <boubacarbeye84@gmail.com>
2026-05-01 15:25:11 +0000

    Setup initial SenTransport avec Header

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import  './App.css'
+import Header from './Header';
+function App() {
+  return(
+    <div className='App'>
+      <Header />
+      <main className="contenu">
+        <p>Bienvenu! Cette application vous aide à trouver 
+          votre ligne de bus à Dakar</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.css';
+function Header() {
+    return(
+        <header className='header'>
+            <h1 className='header-titre'>
+                <p className="header-soustitre">
+                    Votre guide du transport en commun à Dakar
+                </p>
+            </h1>
+        </header>
+    );
+}
+
+export default Header
